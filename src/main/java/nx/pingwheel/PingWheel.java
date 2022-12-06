5a1ef54077e66968e0fd7c1f78643bd6e5493b30
Luken <github@luken.cc>
2022-12-06 19:27:27 +0100

    :tada: init fabric project

---
+++ src/main/java/nx/pingwheel/PingWheel.java
@@ -0,0 +1,10 @@
+package nx.pingwheel;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PingWheel implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/nx/pingwheel/client/PingWheelClient.java
@@ -0,0 +1,13 @@
+package nx.pingwheel.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class PingWheelClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
