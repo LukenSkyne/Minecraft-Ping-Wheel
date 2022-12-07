8e55b81a7a1bc83a83b8e35a16835ec84750590b
Luken <github@luken.cc>
2022-12-07 17:31:02 +0100

    :sparkles: add visual ping indicator

--- src/main/java/nx/pingwheel/client/Core.kt
+++ src/main/java/nx/pingwheel/client/Core.kt
@@ -1,26 +1,26 @@
 package nx.pingwheel.client
 
+import net.minecraft.client.gui.DrawableHelper
 import net.minecraft.client.util.math.MatrixStack
-import net.minecraft.entity.Entity
 import net.minecraft.entity.projectile.ProjectileUtil
-import net.minecraft.util.hit.BlockHitResult
-import net.minecraft.util.hit.EntityHitResult
 import net.minecraft.util.hit.HitResult
-import net.minecraft.util.math.Matrix4f
-import net.minecraft.util.math.Vec2f
-import net.minecraft.util.math.Vec3d
-import net.minecraft.util.math.Vec3f
+import net.minecraft.util.math.*
 import net.minecraft.world.RaycastContext
 import nx.pingwheel.client.util.ConLog
 import nx.pingwheel.client.util.Game
+import nx.pingwheel.client.util.Math
+import nx.pingwheel.client.util.rotateZ
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
+import kotlin.math.PI
+import kotlin.math.pow
 
 object Core {
 
 	private const val REACH_DISTANCE = 256.0
-	private const val HIT_FLUIDS = false
 
 	private var queuePing = false
+	private var pingPos: Vec3d? = null
+	private var pingPosScreen: Vector4f? = null
 
 	@JvmStatic
 	fun doPing() {
@@ -41,7 +41,7 @@ object Core {
 		return Vec3d(temp2)
 	}
 
-	private fun castRayDirectional(direction: Vec3d, tickDelta: Float): HitResult? {
+	private fun castRayDirectional(direction: Vec3d, tickDelta: Float, hitFluids: Boolean): HitResult? {
 		val cameraEntity = Game.cameraEntity
 
 		if (cameraEntity == null || Game.world == null) {
@@ -55,13 +55,15 @@ object Core {
 			.stretch(cameraEntity.getRotationVec(1.0f).multiply(REACH_DISTANCE))
 			.expand(1.0, 1.0, 1.0)
 
-		val blockHitResult = cameraEntity.world.raycast(RaycastContext(
-			rayStartVec,
-			rayEndVec,
-			RaycastContext.ShapeType.OUTLINE,
-			if (HIT_FLUIDS) RaycastContext.FluidHandling.ANY else RaycastContext.FluidHandling.NONE,
-			cameraEntity,
-		))
+		val blockHitResult = cameraEntity.world.raycast(
+			RaycastContext(
+				rayStartVec,
+				rayEndVec,
+				RaycastContext.ShapeType.OUTLINE,
+				if (hitFluids) RaycastContext.FluidHandling.ANY else RaycastContext.FluidHandling.NONE,
+				cameraEntity,
+			)
+		)
 
 		return ProjectileUtil.raycast(
 			cameraEntity,
@@ -73,20 +75,18 @@ object Core {
 		) ?: return blockHitResult
 	}
 
-	@JvmStatic
-	fun onRenderWorld(stack: MatrixStack, projectionMatrix: Matrix4f, tickDelta: Float) {
+	private fun processPing(tickDelta: Float) {
 		if (!queuePing) {
 			return
 		}
 
 		queuePing = false
-
 		val cameraEntity = Game.cameraEntity ?: return
 
 		val scaledWindow = Vec2f(Game.window.scaledWidth.toFloat(), Game.window.scaledHeight.toFloat())
 		val cameraDirection = cameraEntity.getRotationVec(tickDelta)
 		val fov = Game.options.fov.value
-		val angleSize = fov/scaledWindow.y
+		val angleSize = fov / scaledWindow.y
 
 		var verticalRotationAxis = Vec3f(cameraDirection)
 		verticalRotationAxis.cross(Vec3f.POSITIVE_Y)
@@ -113,24 +113,77 @@ object Core {
 			scaledWindow.y.toInt(),
 		)
 
-		val hitResult = castRayDirectional(direction, tickDelta)
+		val hitResult = castRayDirectional(direction, tickDelta, cameraEntity.isSneaking)
 
-		// when (val hitResult = castDirectionalRay(direction, tickDelta)?.type) {
-		when (hitResult?.type) {
+		pingPos = when (hitResult?.type) {
 			HitResult.Type.BLOCK -> {
-				ConLog.info("HitResult.Type.BLOCK")
+				hitResult.pos
 			}
+
 			HitResult.Type.ENTITY -> {
-				ConLog.info("HitResult.Type.ENTITY")
+				hitResult.pos
 			}
+
 			else -> {
-				ConLog.info("HitResult.Type.MISS")
+				null
 			}
 		}
 	}
 
 	@JvmStatic
-	fun onRenderGUI(stack: MatrixStack, ci: CallbackInfo) {
+	fun onRenderWorld(stack: MatrixStack, projectionMatrix: Matrix4f, tickDelta: Float) {
+		val modelViewMatrix = stack.peek().positionMatrix
 
+		processPing(tickDelta)
+
+		val pingPos = pingPos
+		pingPosScreen = if (pingPos != null) Math.project3Dto2D(pingPos, modelViewMatrix, projectionMatrix) else null
+	}
+
+	@JvmStatic
+	fun onRenderGUI(stack: MatrixStack, ci: CallbackInfo) {
+		val pingPosScreen = pingPosScreen
+
+		if (pingPosScreen != null) {
+			val cameraPosVec = Game.player?.getCameraPosVec(Game.tickDelta)
+			val distanceToPing = cameraPosVec?.distanceTo(pingPos) ?: 0.0
+			val pingScaleMin = 0.25f
+
+			var pingScale = (1 / (distanceToPing.pow(0.3))).toFloat()
+
+			if (pingScale < pingScaleMin)
+				pingScale = pingScaleMin
+
+			stack.push() // push
+			val white = -0x1
+			//val black = -0x1000000
+
+			val uiScale = Game.window.scaleFactor
+			stack.translate(
+				pingPosScreen.x / uiScale,
+				pingPosScreen.y / uiScale,
+				0.0
+			)
+
+			stack.scale(pingScale, pingScale, 1f)
+
+			stack.push() // push text
+			val text = "%.1fm".format(distanceToPing)
+			val textWidth: Int = Game.textRenderer.getWidth(text)
+			val textHeight: Int = Game.textRenderer.fontHeight
+			stack.translate((-textWidth * 0.5f).toDouble(), (-textHeight * 2.5f).toDouble(), 0.0)
+			DrawableHelper.fill(stack, -2, -2, textWidth + 1, textHeight, 0x40000000)
+			Game.textRenderer.draw(stack, text, 0f, 0f, white)
+			stack.pop() // pop text
+
+			stack.rotateZ(PI.toFloat() / 4f)
+			stack.translate(
+				-2.5,
+				-2.5,
+				0.0
+			)
+			DrawableHelper.fill(stack, 0, 0, 5, 5, white)
+			stack.pop() // pop
+		}
 	}
 }
