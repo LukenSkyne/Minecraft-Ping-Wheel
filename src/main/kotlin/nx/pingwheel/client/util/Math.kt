53ef864b4797a3ad42530986b2fe40dfe9395fb7
Luken <github@luken.cc>
2022-12-17 00:14:03 +0100

    :fire: remove draw helper class

--- src/main/kotlin/nx/pingwheel/client/util/Math.kt
+++ src/main/kotlin/nx/pingwheel/client/util/Math.kt
@@ -37,13 +37,4 @@ object Math {
 
 		return Vector4f(x, wnd.height - y, screenCoords.z, 1f / (screenCoords.w * 2f))
 	}
-
-	fun hexToVec(color: Long): Vector4f {
-		val a: Float = (color shr 24 and 0xFF) / 255f
-		val r: Float = (color shr 16 and 0xFF) / 255f
-		val g: Float = (color shr 8 and 0xFF) / 255f
-		val b: Float = (color and 0xFF) / 255f
-
-		return Vector4f(r, g, b, a)
-	}
 }
