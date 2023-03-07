f23ed3114b4e6f309a4a6b79c864ed357cdd44f6
Luken <github@luken.cc>
2023-03-07 18:05:56 +0100

    :sparkles: add client command to change and display current channel

--- src/main/java/nx/pingwheel/client/PingWheelClient.java
+++ src/main/java/nx/pingwheel/client/PingWheelClient.java
@@ -1,18 +1,26 @@
 package nx.pingwheel.client;
 
+import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.LiteralText;
 import nx.pingwheel.shared.Constants;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.lwjgl.glfw.GLFW;
+
+import java.util.function.Function;
+
+import static net.fabricmc.fabric.api.client.command.v1.ClientCommandManager.argument;
+import static net.fabricmc.fabric.api.client.command.v1.ClientCommandManager.literal;
 
 @Environment(EnvType.CLIENT)
 public class PingWheelClient implements ClientModInitializer {
@@ -26,9 +34,10 @@ public class PingWheelClient implements ClientModInitializer {
 	public void onInitializeClient() {
 		LOGGER.info("[Ping-Wheel] Client Init");
 
-		SetupKeyBindings();
-
 		PingWheelConfigHandler.getInstance().load();
+
+		SetupKeyBindings();
+		SetupClientCommands();
 
 		ClientPlayNetworking.registerGlobalReceiver(Constants.S2C_PING_LOCATION, Core::onReceivePing);
 	}
@@ -46,5 +55,37 @@ public class PingWheelClient implements ClientModInitializer {
 				MinecraftClient.getInstance().setScreen(new PingWheelSettingsScreen());
 			}
 		});
+	}
+
+	private void SetupClientCommands() {
+		Function<String, String> formatChannel = (channel) -> "".equals(channel) ? "§eGlobal §7(default)" : String.format("\"§6%s§r\"", channel);
+
+		var cmdChannel = literal("channel")
+				.executes((context) -> {
+					var currentChannel = PingWheelConfigHandler.getInstance().getConfig().getChannel();
+					context.getSource().sendFeedback(new LiteralText(String.format("Current Ping-Wheel channel: %s", formatChannel.apply(currentChannel))));
+
+					return 1;
+				})
+				.then(argument("channel_name", StringArgumentType.string()).executes((context) -> {
+					var newChannel = context.getArgument("channel_name", String.class);
+
+					PingWheelConfigHandler.getInstance().getConfig().setChannel(newChannel);
+					PingWheelConfigHandler.getInstance().save();
+
+					context.getSource().sendFeedback(new LiteralText(String.format("Set Ping-Wheel channel to: %s", formatChannel.apply(newChannel))));
+
+					return 1;
+				}));
+
+		var cmd = literal("pingwheel")
+				.executes((context) -> {
+					context.getSource().sendFeedback(new LiteralText("/pingwheel channel [<channel_name>]"));
+
+					return 1;
+				})
+				.then(cmdChannel);
+
+		ClientCommandManager.DISPATCHER.register(cmd);
 	}
 }
