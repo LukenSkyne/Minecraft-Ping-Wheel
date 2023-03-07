7f4fcb017fa32b6fa6e131ecb323ff7ccd83cc79
Luken <github@luken.cc>
2023-03-07 16:02:16 +0100

    :children_crossing: change reach distance to 1024

--- src/main/kotlin/nx/pingwheel/client/Core.kt
+++ src/main/kotlin/nx/pingwheel/client/Core.kt
@@ -29,7 +29,7 @@ import kotlin.math.pow
 
 object Core {
 
-	private const val REACH_DISTANCE = 256.0
+	private const val REACH_DISTANCE = 1024.0
 	private const val TPS = 20
 
 	private val config = PingWheelConfigHandler.getInstance().config
