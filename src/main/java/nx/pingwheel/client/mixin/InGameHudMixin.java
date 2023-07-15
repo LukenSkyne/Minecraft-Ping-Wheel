76badd9df81e47884928621a917a60b688caa027
Luken <github@luken.cc>
2023-07-15 12:27:25 +0200

    :sparkles: add mixins

---
+++ src/main/java/nx/pingwheel/client/mixin/InGameHudMixin.java
@@ -0,0 +1,22 @@
+package nx.pingwheel.client.mixin;
+
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
+import nx.pingwheel.client.ClientCore;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public abstract class InGameHudMixin {
+
+    @Inject(method = "render", at = @At(
+            value = "FIELD",
+            target = "Lnet/minecraft/client/option/GameOptions;debugEnabled:Z",
+            opcode = Opcodes.GETFIELD, args = {"log=false"}))
+    private void onRenderPreDebugScreen(MatrixStack stack, float tickDelta, CallbackInfo ci) {
+        ClientCore.onRenderGUI(stack, tickDelta);
+    }
+}
---
+++ src/main/java/nx/pingwheel/client/mixin/WorldRendererMixin.java
@@ -0,0 +1,24 @@
+package nx.pingwheel.client.mixin;
+
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.LightmapTextureManager;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.Matrix4f;
+import nx.pingwheel.client.ClientCore;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(WorldRenderer.class)
+public abstract class WorldRendererMixin {
+
+    @Inject(method = "render",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;renderWorldBorder(Lnet/minecraft/client/render/Camera;)V",
+                    shift = At.Shift.AFTER))
+    private void onRenderPostWorldBorder(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f positionMatrix, CallbackInfo ci) {
+        ClientCore.onRenderWorld(matrices, positionMatrix, tickDelta);
+    }
+}
