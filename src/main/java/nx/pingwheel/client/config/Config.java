a5dab916a24d62382c72cc52bef8f1b688921412
Luken <github@luken.cc>
2023-07-15 12:25:14 +0200

    :sparkles: add config + handler

---
+++ src/main/java/nx/pingwheel/client/config/Config.java
@@ -0,0 +1,18 @@
+package nx.pingwheel.client.config;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Config {
+	int pingVolume = 100;
+	int pingDistance = 2048;
+	boolean itemIconVisible = true;
+	int pingDuration = 7;
+	String channel = "";
+}
---
+++ src/main/java/nx/pingwheel/client/config/ConfigHandler.java
@@ -0,0 +1,92 @@
+package nx.pingwheel.client.config;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import net.fabricmc.loader.api.FabricLoader;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static nx.pingwheel.shared.PingWheel.LOGGER;
+
+public class ConfigHandler {
+
+	final private Gson gson;
+	final private Path configPath;
+	final private String configName;
+
+	private int configHash;
+	private Config config;
+
+	public ConfigHandler(String configName) {
+		this.gson = new GsonBuilder().setPrettyPrinting().create();
+		this.configPath = FabricLoader.getInstance().getConfigDir();
+		this.configName = configName;
+
+		this.configHash = 0;
+		this.config = new Config();
+	}
+
+	public Config getConfig() {
+		return config;
+	}
+
+	public void save() {
+		if (configHash == config.hashCode()) {
+			return;
+		}
+
+		var configFile = configPath.resolve(configName).toFile();
+
+		if (!configFile.exists()) {
+			configFile.getParentFile().mkdirs();
+
+			try {
+				Files.createFile(configFile.toPath());
+			} catch (IOException e) {
+				LOGGER.error("Creating Config failed: " + e);
+				return;
+			}
+		}
+
+		try {
+			var writer = Files.newBufferedWriter(configFile.toPath());
+			gson.toJson(config, writer);
+			writer.close();
+		} catch (Exception e) {
+			LOGGER.error("Saving Config failed: " + e);
+			return;
+		}
+
+		configHash = config.hashCode();
+		LOGGER.info("Saved " + config);
+	}
+
+	public void load() {
+		var configFile = configPath.resolve(configName).toFile();
+
+		if (!configFile.exists()) {
+			save();
+			return;
+		}
+
+		try {
+			var reader = Files.newBufferedReader(configFile.toPath());
+			config = gson.fromJson(reader, Config.class);
+		} catch (Exception e) {
+			config = null;
+		}
+
+		if (config == null) {
+			config = new Config();
+			LOGGER.error("Config is broken -> reset to defaults");
+
+			save();
+			return;
+		}
+
+		configHash = config.hashCode();
+		LOGGER.info("Loaded " + config);
+	}
+}
