257b421de507bc0d9c040bdbf3c27473312a610f
Luken <github@luken.cc>
2023-12-11 22:25:50 +0100

    :construction: add helper class to wrap option creation

---
+++ common/src/main/java/nx/pingwheel/common/helper/OptionUtils.java
@@ -0,0 +1,37 @@
+package nx.pingwheel.common.helper;
+
+import net.minecraft.client.option.CyclingOption;
+import net.minecraft.client.option.DoubleOption;
+import net.minecraft.client.option.Option;
+import net.minecraft.text.Text;
+
+import java.util.function.*;
+
+public interface OptionUtils {
+
+	static Option ofInt(String key, int min, int max, int step, Function<Integer, Text> formatter, Supplier<Integer> getter, Consumer<Integer> setter) {
+		return new DoubleOption(
+			key, min, max, step,
+			(gameOptions) -> (double)getter.get(),
+			(gameOptions, value) -> setter.accept(value.intValue()),
+			(gameOptions, option) -> formatter.apply(getter.get())
+		);
+	}
+
+	static Option ofFloat(String key, float min, float max, float step, Function<Float, Text> formatter, Supplier<Float> getter, Consumer<Float> setter) {
+		return new DoubleOption(
+			key, min, max, step,
+			(gameOptions) -> (double)getter.get(),
+			(gameOptions, value) -> setter.accept(value.floatValue()),
+			(gameOptions, option) -> formatter.apply(getter.get())
+		);
+	}
+
+	static Option ofBool(String key, Supplier<Boolean> getter, Consumer<Boolean> setter) {
+		return CyclingOption.create(
+			key,
+			(gameOptions) -> getter.get(),
+			(gameOptions, option, value) -> setter.accept(value)
+		);
+	}
+}
