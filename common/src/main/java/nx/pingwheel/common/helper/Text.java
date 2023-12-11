bd9bb0d8234333fff906497427e1c1bfd75c6dd6
Luken <github@luken.cc>
2023-12-11 22:11:44 +0100

    :construction: add helper class to wrap text creation

---
+++ common/src/main/java/nx/pingwheel/common/helper/Text.java
@@ -0,0 +1,16 @@
+package nx.pingwheel.common.helper;
+
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.TranslatableText;
+
+public interface Text {
+
+	static MutableText empty() {
+		return (MutableText)LiteralText.EMPTY;
+	}
+
+	static MutableText translatable(String key, Object... args) {
+		return new TranslatableText(key, args);
+	}
+}
