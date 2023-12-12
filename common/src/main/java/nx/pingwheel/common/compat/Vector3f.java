88c6e0f90c72804e50c379f7f9d3b94784b4a3a8
Luca v. d. W <github@luken.cc>
2023-12-13 00:12:03 +0100

    :construction: add vector compat classes

---
+++ common/src/main/java/nx/pingwheel/common/compat/Vector3f.java
@@ -0,0 +1,14 @@
+package nx.pingwheel.common.compat;
+
+public class Vector3f {
+
+	public float x;
+	public float y;
+	public float z;
+
+	public Vector3f(float x, float y, float z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
