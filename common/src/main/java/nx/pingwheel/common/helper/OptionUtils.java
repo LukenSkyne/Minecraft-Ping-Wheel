33887827ceeff10b0ae8a135c21865e681f5ed1f
Luca v. d. W <github@luken.cc>
2024-05-09 07:08:23 +0200

    :alien: migrate v1.9.0 to 1.19.3

--- common/src/main/java/nx/pingwheel/common/helper/OptionUtils.java
+++ common/src/main/java/nx/pingwheel/common/helper/OptionUtils.java
@@ -1,8 +1,7 @@
 package nx.pingwheel.common.helper;
 
-import net.minecraft.client.CycleOption;
-import net.minecraft.client.Option;
-import net.minecraft.client.ProgressOption;
+import com.mojang.serialization.Codec;
+import net.minecraft.client.OptionInstance;
 import net.minecraft.network.chat.Component;
 
 import java.util.function.Consumer;
@@ -12,29 +11,40 @@ import java.util.function.Supplier;
 public class OptionUtils {
 	private OptionUtils() {}
 
-	public static Option ofInt(String key, int min, int max, int step, Function<Integer, Component> formatter, Supplier<Integer> getter, Consumer<Integer> setter) {
-		return new ProgressOption(
-			key, min, max, step,
-			(gameOptions) -> (double)getter.get(),
-			(gameOptions, value) -> setter.accept(value.intValue()),
-			(gameOptions, option) -> formatter.apply(getter.get())
-		);
-	}
-
-	public static Option ofFloat(String key, float min, float max, float step, Function<Float, Component> formatter, Supplier<Float> getter, Consumer<Float> setter) {
-		return new ProgressOption(
-			key, min, max, step,
-			(gameOptions) -> (double)getter.get(),
-			(gameOptions, value) -> setter.accept(value.floatValue()),
-			(gameOptions, option) -> formatter.apply(getter.get())
-		);
-	}
-
-	public static Option ofBool(String key, Supplier<Boolean> getter, Consumer<Boolean> setter) {
-		return CycleOption.createOnOff(
+	public static OptionInstance<Integer> ofInt(String key, int min, int max, int step, Function<Integer, Component> formatter, Supplier<Integer> getter, Consumer<Integer> setter) {
+		return new OptionInstance<>(
 			key,
-			(gameOptions) -> getter.get(),
-			(gameOptions, option, value) -> setter.accept(value)
+			OptionInstance.noTooltip(),
+			(optionText, value) -> formatter.apply(getter.get()),
+			(new OptionInstance.IntRange(min / step, max / step))
+				.xmap((value) -> value * step, (value) -> value / step),
+			Codec.intRange(min, max),
+			getter.get(),
+			setter
+		);
+	}
+
+	public static OptionInstance<Float> ofFloat(String key, float min, float max, float step, Function<Float, Component> formatter, Supplier<Float> getter, Consumer<Float> setter) {
+		var iMin = (int) (min / step);
+		var iMax = (int) (max / step);
+
+		return new OptionInstance<>(
+			key,
+			OptionInstance.noTooltip(),
+			(optionText, value) -> formatter.apply(getter.get()),
+			(new OptionInstance.IntRange(iMin, iMax))
+				.xmap((value) -> value * step, (value) -> (int) (value / step)),
+			Codec.floatRange(iMin, iMax),
+			getter.get(),
+			setter
+		);
+	}
+
+	public static OptionInstance<Boolean> ofBool(String key, Supplier<Boolean> getter, Consumer<Boolean> setter) {
+		return OptionInstance.createBoolean(
+			key,
+			getter.get(),
+			setter
 		);
 	}
 }
