dee557443ec1a2a48032ae87b6eac763a33b1fa4
Luca v. d. W. <github@luken.cc>
2026-09-20 16:31:14 +0200

    :bug: fix invalid default key for migration to SDL (#93)

--- common/src/main/java/nx/pingwheel/common/util/InputUtils.java
+++ common/src/main/java/nx/pingwheel/common/util/InputUtils.java
@@ -14,7 +14,7 @@ public class InputUtils {
 
 	private static final KeyMapping.Category SETTINGS_CATEGORY = KeyMapping.Category.register(Identifier.fromNamespaceAndPath(MOD_ID, "name"));
 	public static final KeyMapping KEY_BINDING_PING = new KeyMapping(LanguageUtils.keyOf("key", "ping_location"), InputConstants.Type.MOUSE, InputConstants.MOUSE_BUTTON_5, SETTINGS_CATEGORY);
-	public static final KeyMapping KEY_BINDING_SETTINGS = new KeyMapping(LanguageUtils.keyOf("key", "open_settings"), InputConstants.Type.KEYBOARD, -1, SETTINGS_CATEGORY);
+	public static final KeyMapping KEY_BINDING_SETTINGS = new KeyMapping(LanguageUtils.keyOf("key", "open_settings"), InputConstants.Type.KEYBOARD, InputConstants.UNKNOWN.getValue(), SETTINGS_CATEGORY);
 
 	public static boolean consumePingHotkey() {
 		if (!KEY_BINDING_PING.same(Game.options.keyPickItem)) {
